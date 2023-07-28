88f3dcf26903d5fb13bf10ce19e1f4b05bab2f5b
Alexander Kun <alexinyamagata@gmail.com>
2023-07-28 09:12:54 +0900

    fix copilot

--- .config/nvim/lua/nvim-v1/remap.lua
+++ .config/nvim/lua/nvim-v1/remap.lua
@@ -49,5 +49,6 @@ end)
 
 -- Ctrl B for copilot completion
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-b>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.g.copilot_assume_mapped = true
+vim.api.nvim_set_keymap("i", "<C-B>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
