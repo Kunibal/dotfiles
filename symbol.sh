cf770baa40b92af87a2a3526c80b075be5d99917
Alexander Kun <alexinyamagata@gmail.com>
2023-08-11 12:23:24 +0900

    add symbolic link file

---
+++ symbol.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p ~/.config/nvim
+ln -sf ~/pc-settings/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/pc-settings/.zshrc ~/.zshrc
+ln -sf ~/pc-settings/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+ln -sf ~/pc-settings/.alacritty.yml ~/.config/alacritty/.alacritty.yml
