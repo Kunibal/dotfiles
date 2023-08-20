531e68d868eea5f4301ec50957765c997ebc4613
Alexander Kun <alexinyamagata@gmail.com>
2023-08-20 20:48:57 +0900

    edit symlinks

--- symbol.sh
+++ symbol.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.config/nvim
 ln -sf ~/pc-settings/.config/nvim/init.lua ~/.config/nvim/init.lua
-ln -sf ~/pc-settings/.config/nvim/after ~/.config/nvim/
+ln -sf ~/pc-settings/.config/nvim/after ~/.config/nvim
 ln -sf ~/pc-settings/.zshrc ~/.zshrc
 ln -sf ~/pc-settings/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
-ln -sf ~/pc-settings/.alacritty.yml ~/.config/alacritty/.alacritty.yml
+ln -sf ~/pc-settings/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
