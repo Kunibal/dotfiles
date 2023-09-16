96209cfceb0f3ed6470ca9216db41f05594e5ec0
Alexander Kun <alexinyamagata@gmail.com>
2023-09-16 23:08:58 +0900

    add rust and bun installation

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 xcode-select --install
+
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl -fsSL https://bun.sh/install | bash
+
 brew install tmux
 brew install alacritty
 brew install neovim
@@ -8,5 +13,3 @@ brew install ripgrep
 brew install node
 brew install exa
 brew install zsh-autosuggestions
-
-curl -sSL https://get.rvm.io | bash -s stable --ruby
