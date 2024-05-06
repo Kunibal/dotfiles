1a41f1a2b35ebc4eb4f7c1b95541068d66df01f2
Alexander Kun <alexinyamagata@gmail.com>
2024-05-06 17:11:06 +0900

    migrate from yml to toml (alacritty)

--- linux/symbol.sh
+++ linux/symbol.sh
@@ -9,4 +9,4 @@ ln -sf ~/pc-settings/.config/nvim/lua ~/.config/nvim
 ln -sf ~/pc-settings/.config/nvim/.stylua.toml ~/.config/nvim/.stylua.toml
 ln -sf ~/pc-settings/linux/.zshrc ~/.zshrc
 ln -sf ~/pc-settings/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
-ln -sf ~/pc-settings/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/pc-settings/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
