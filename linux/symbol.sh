4e6ca3cc870311bc40da82e0f5c4a37d96a0872c
Alexander Kun <alexinyamagata@gmail.com>
2025-01-27 13:41:54 +0900

    update

--- linux/symbol.sh
+++ linux/symbol.sh
@@ -5,11 +5,11 @@ mkdir -p ~/.config/tmux
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/ghostty
 
-ln -sf ~/pc-settings/.config/nvim/init.lua ~/.config/nvim/init.lua
-ln -sf ~/pc-settings/.config/nvim/lua ~/.config/nvim
-ln -sf ~/pc-settings/.config/nvim/.stylua.toml ~/.config/nvim/.stylua.toml
-ln -sf ~/pc-settings/linux/.zshrc ~/.zshrc
-ln -sf ~/pc-settings/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
-ln -sf ~/pc-settings/.config/tmux/tmux_session.sh ~/.config/tmux/tmux_session.sh
-ln -sf ~/pc-settings/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
-ln -sf ~/pc-settings/.config/ghostty/config ~/.config/ghostty/config
+ln -sf ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/dotfiles/.config/nvim/lua ~/.config/nvim
+ln -sf ~/dotfiles/.config/nvim/.stylua.toml ~/.config/nvim/.stylua.toml
+ln -sf ~/dotfiles/linux/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+ln -sf ~/dotfiles/.config/tmux/tmux_session.sh ~/.config/tmux/tmux_session.sh
+ln -sf ~/dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -sf ~/dotfiles/.config/ghostty/config ~/.config/ghostty/config
