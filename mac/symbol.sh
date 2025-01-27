6716e1f8fb2583dd4da96782fc0831e0b633292c
Alexander Kun <alexinyamagata@gmail.com>
2025-01-27 13:44:02 +0900

    update

--- mac/symbol.sh
+++ mac/symbol.sh
@@ -4,9 +4,9 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.config/tmux
 mkdir -p ~/.config/alacritty
 
-ln -sf ~/pc-settings/.config/nvim/init.lua ~/.config/nvim/init.lua
-ln -sf ~/pc-settings/.config/nvim/lua ~/.config/nvim
-ln -sf ~/pc-settings/.config/nvim/.stylua.toml ~/.config/nvim/.stylua.toml
-ln -sf ~/pc-settings/mac/.zshrc ~/.zshrc
-ln -sf ~/pc-settings/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
-ln -sf ~/pc-settings/.config/alacritty/mac/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/dotfiles/.config/nvim/lua ~/.config/nvim
+ln -sf ~/dotfiles/.config/nvim/.stylua.toml ~/.config/nvim/.stylua.toml
+ln -sf ~/dotfiles/mac/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+ln -sf ~/dotfiles/.config/alacritty/mac/alacritty.yml ~/.config/alacritty/alacritty.yml
